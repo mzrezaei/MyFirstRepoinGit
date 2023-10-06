218ec1012d4620ad4f313d5a3c9cf8b1d9c4dcb7
Marzieh Rezaei <marziehrezaei930@gmail.com>
2023-10-06 09:17:40 +0330

    interface

---
+++ ConsoleApp2/IMyVehicle.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    interface IMyVehicle
+    {
+        void PassangerNUm();
+    }
+    interface IAnimal
+    {
+        void animalSound(); // interface method (does not have a body)
+    }
+
+    // Pig "implements" the IAnimal interface
+    class Car : IMyVehicle
+    {
+        public void PassangerNUm()
+        {
+            Console.WriteLine("Car contanins up to 4 passangers");
+        }
+    }
+
+    class Van : IMyVehicle
+    {
+        public void PassangerNUm()
+        {
+            Console.WriteLine("Van contanins up to 10 passangers");
+        }
+    }
+    class Bus : IMyVehicle
+    {
+        public void PassangerNUm()
+        {
+            Console.WriteLine("Bus contanins up to 20 passangers");
+        }
+    }
+
+    
+}
--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -7,29 +7,21 @@ using static ConsoleApp2.Program;
 
 namespace ConsoleApp2
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Passengers();
+            Car myCar = new Car();  
+            Van myVan = new Van();
+            Bus myBus = new Bus();
+
+            myCar.PassangerNUm();
+            myVan.PassangerNUm();
+            myBus.PassangerNUm();
             Console.ReadKey();
         }
-        static void Passengers()
-        {
-            Console.WriteLine("Please type one of these Itmes: Car, Van, Bus");
-            string CarType = Console.ReadLine().ToLower();
-             if (CarType == "car")
-                Console.WriteLine("Passangers number is 4");
-            else if (CarType == "van" )
-                Console.WriteLine("Passangers number is 10");
-            else if (CarType == "bus")
-                Console.WriteLine("Passangers number is 20");
-            else
-                Console.WriteLine("Please type correct car name");
-            
-        }
+
         
-                
     }
 
 }
